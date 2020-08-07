0bae5baab429617600277a4c4bec9e3909cbdf35
leo-holanda <leo.uldholanda@gmail.com>
2020-08-06 21:27:14 -0300

    Add initial express code

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var nunjucks = require('nunjucks')
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.set('view engine', 'njk')
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
