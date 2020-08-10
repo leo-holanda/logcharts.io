27cdcb1d0f5719e0efa93ae3940aaa7872a54571
leo-holanda <leo.uldholanda@gmail.com>
2020-08-10 18:53:14 -0300

    Add prettier

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var nunjucks = require('nunjucks')
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var nunjucks = require("nunjucks");
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
-nunjucks.configure('views', {
+nunjucks.configure("views", {
   autoescape: true,
   express: app,
-  watch: true
-})
+  watch: true,
+});
 
-app.set('view engine', 'njk')
-app.use(logger('dev'));
+app.set("view engine", "njk");
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 module.exports = app;
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,26 +1,26 @@
 // Each field in the csv file becomes a button
-function createButtons(fields){
-  let btn
-  for (field of fields){    
-    if(field){
-      btn = document.createElement("button")
-      btn.innerHTML = field 
-      document.body.appendChild(btn)     
+function createButtons(fields) {
+  let btn;
+  for (field of fields) {
+    if (field) {
+      btn = document.createElement("button");
+      btn.innerHTML = field;
+      document.body.appendChild(btn);
     }
   }
 }
 
 // When user sends csv...
-document.getElementById("log_input").addEventListener('change', () => {
-  const log = document.getElementById("log_input").files[0]
+document.getElementById("log_input").addEventListener("change", () => {
+  const log = document.getElementById("log_input").files[0];
 
   // Parse the csv and process the results
   Papa.parse(log, {
     header: true,
-    encoding: 'latin3', // Important for degree symbol
+    encoding: "latin3", // Important for degree symbol
     skipEmptyLines: true,
-    complete: function(results){
-      createButtons(results.meta.fields)
-    }
-  })  
-})
+    complete: function (results) {
+      createButtons(results.meta.fields);
+    },
+  });
+});
