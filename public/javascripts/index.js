fc670786c5f32d3581a3a5b9f6ff1ee4b562a785
leo-holanda <leo.uldholanda@gmail.com>
2020-08-19 21:38:48 -0300

    Add container for buttons and improve CSS

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,30 @@
+//Remove form so buttons and graph can be displayed
+function removeForm() {
+  document.querySelector("form").remove();
+}
+
+//Create a container for field buttons
+function createBtnContainer() {
+  document.querySelector("main").classList.remove("aligned");
+  let div = document.createElement("div");
+  div.classList.add("btn-container");
+  document.querySelector("main").appendChild(div);
+
+  let title = document.createElement("a");
+  title.innerHTML = "FIELDS";
+  title.classList.add("btn-container-title");
+  document.querySelector(".btn-container").appendChild(title);
+}
+
 // Each field in the csv file becomes a button
 function createButtons(fields) {
   let btn;
   for (field of fields) {
-    if (field) {
+    if (field && !field.includes("Time") && !field.includes("Date")) {
       btn = document.createElement("button");
       btn.innerHTML = field;
-      document.querySelector("main").appendChild(btn);
+      btn.classList.add("field-btn");
+      document.querySelector(".btn-container").appendChild(btn);
     }
   }
 }
@@ -20,6 +39,8 @@ document.getElementById("log_input").addEventListener("change", () => {
     encoding: "latin3", // Important for degree symbol
     skipEmptyLines: true,
     complete: function (results) {
+      removeForm();
+      createBtnContainer();
       createButtons(results.meta.fields);
     },
   });
