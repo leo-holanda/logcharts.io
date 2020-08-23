429aaec49869fc8ae179666318f9cc2a574031de
leo-holanda <leo.uldholanda@gmail.com>
2020-08-22 22:44:06 -0300

    Add update chart by selecting field btn

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -4,74 +4,82 @@ function fixTime(time){
 	
 	//If time is a missing value
     if(time){
-        let data = time.split(":")
+        const data = time.split(":")
 
-        let new_time = new Date()
-        new_time.setHours(data[0])
-        new_time.setMinutes(data[1])
-        new_time.setSeconds(data[2])
+        const newTime = new Date()
+        newTime.setHours(data[0])
+        newTime.setMinutes(data[1])
+        newTime.setSeconds(data[2])
 	
 		//This validation is required because sometimes the string "Time" (header name) appears between the times
-		if(!isNaN(new_time.getTime())){
-			return new_time
+		if(!isNaN(newTime.getTime())){
+			return newTime
 		}
     }
 }
 
-//To remove missing values or strings with the PC component name
+//Check if value ins't missing, convert Yes/No to 1/0 if necesssary or return the value in float
 function checkValue(value){
-    if(value && !isNaN(value)){
-        return value
+    if(value){
+        if(value == "No"){
+            return 0
+        }
+        else if(value == "Yes"){
+            return 1
+        }
+        else if(!isNaN(value)){
+            return parseFloat(value)
+        }
     }
-    return 0
+    //return undefinied otherwise
 }
 
 function createChart(data){
 
 	//Define the chart size
-    var containerHeight = document.querySelector(".chart-container").clientHeight
-    var containerWidth = document.querySelector(".chart-container").clientWidth
+    const containerHeight = document.querySelector(".chart-container").clientHeight
+    const containerWidth = document.querySelector(".chart-container").clientWidth
 
-    var margin = {top: 50, right: 50, bottom: 50, left: 50}
+    //Define chart margin
+    const margin = {top: 50, right: 50, bottom: 50, left: 50}
     , width = containerWidth - margin.left - margin.right
     , height = containerHeight - margin.top - margin.bottom;
 	
 	//Define the scale of x
-    x = d3.scaleTime()
+    const x = d3.scaleTime()
     .domain(d3.extent(data, d => fixTime(d["Time"])))
     .range([margin.left + 10, width - margin.right]) // + 10 so the line doesn't touch the y axis
 	
 	//Define the x axis
-    xAxis = g => g
+    const xAxis = g => g
         .attr("transform", `translate(0,${height - margin.bottom})`)
+        .transition()
+        .duration(1000)
         .call(d3.axisBottom(x).tickSizeOuter(0))
 
 	//Define the scale of y
-    y = d3.scaleLinear()
+    let y = d3.scaleLinear()
         .domain(d3.extent(data, d => checkValue(d["CPU [°C]"]))).nice()
         .range([height - margin.bottom, margin.top])
 
 	//Define the y axis
-    yAxis = g => g
+    let yAxis = g => g
         .attr("transform", `translate(${margin.left},0)`)
+        .transition()
+        .duration(1000)
         .call(d3.axisLeft(y))
         .call(g => g.select(".domain").remove())
-        .call(g => g.select(".tick:last-of-type text").clone()
-            .attr("x", 3)
-            .attr("text-anchor", "start")
-            .attr("font-weight", "bold")
-            .text(data["CPU [°C]"]))
+        .attr("class", "y-axis")
 
 	//Define the chart line that will be drawn. The x point is the Time value and the Y point is the CPU temperature value
-    line = d3.line()
-        .defined(d => !isNaN(d["CPU [°C]"]))
+    let line = d3.line()
         .x(d => x(fixTime(d["Time"])))
         .y(d => y(checkValue(d["CPU [°C]"])))
-	
+
 	//Append the svg element to the chart container
     const svg = d3.select(".chart-container").append("svg")
 		.attr("viewBox", `0 0 ${width} ${height}`)
-	
+    
 	//Append the x Axis
     svg.append("g")
 		.call(xAxis);
@@ -79,6 +87,15 @@ function createChart(data){
 	//Append the y Axis
     svg.append("g")
         .call(yAxis);
+    
+    //Append the grid line in y axis
+    svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", `translate(${margin.left}, 0)`)
+        .call(d3.axisLeft(y)
+            .tickSize((-width) + (margin.right * 2))
+            .tickFormat(""))
+        .call(g => g.select(".domain").remove())
 	
 	//Append the path that contains the chart line
     svg.append("path")
@@ -88,6 +105,42 @@ function createChart(data){
         .attr("stroke-width", 1.5)
         .attr("stroke-linejoin", "round")
         .attr("stroke-linecap", "round")
-		.attr("d", line);
-}
+        .attr("class", "line")
+        .attr("d", line);
 
+    //Update chart with the specified field data
+    function updateChart(field){
+
+        //Update domain of y scale
+        y.domain(d3.extent(data, d => checkValue(d[field]))).nice()
+        //Select all y axis and update values by calling yAxis
+        svg.selectAll(".y-axis")
+            .transition()
+            .duration(1000)
+            .call(yAxis)
+        
+        //Select all grid lines in y axis and update positions
+        svg.selectAll(".grid")
+            .attr("transform", `translate(${margin.left}, 0)`)
+            .call(d3.axisLeft(y)
+                .tickSize((-width) + (margin.right * 2))
+                .tickFormat(""))
+            .call(g => g.select(".domain").remove())
+
+        //After update the y-axis, update the line in path object with new data
+        svg.selectAll(".line")
+            .transition()
+            .duration(1000)
+            .attr("d", d3.line()
+                .x(d => x(fixTime(d["Time"])))
+                .y(d => y(checkValue(d[field]))))
+    }
+
+    //When user click in a field button, update chart with field data
+    document.querySelector(".btn-container").addEventListener('click', function(event){
+        if(event.target.className == "field-btn"){
+            let selectedField = event.target.innerHTML
+            updateChart(selectedField)
+        }
+    })
+}
